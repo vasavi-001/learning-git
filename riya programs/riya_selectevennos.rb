5fd1ca582700772c6665e16baf5888b0104fafae
RIYA <riya.saxena@reddoorz.com>
2024-02-20 11:40:47 +0530

    riyas program

---
+++ riya programs/riya_selectevennos.rb
@@ -0,0 +1,9 @@
+
+# 4 SELECT NUMBER THAT ARE EVEN
+
+a= [2,3,4,5,6,7,8]
+for b in a
+    if b %2 ==0
+        puts b
+    end
+end
---
+++ riya programs/riya_sumofelements.rb
@@ -0,0 +1,8 @@
+# SUM OF ARRAYS
+ 
+s = 0
+arr = [1,4,6,7]
+arr.each do |i|
+    s += i
+end
+puts s
